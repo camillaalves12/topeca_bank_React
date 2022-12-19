8dd24db1b9d4ba8b52a0b05df5b4a9502071bfde
Camilla Alves <camillaalves285@gmail.com>
2022-12-19 20:11:37 -0300

    aaaa

--- src/paginas/currencyConverter/CurrencyConverter.js
+++ src/paginas/currencyConverter/CurrencyConverter.js
@@ -39,31 +39,23 @@ export default function CurrencyConverter() {
             
             }
 
-
-
-
    return(
       <>
          <Header />
 
          <h1 className="titleConversor">Conversor de Moedas</h1>
-
          <div className="caixas">
-         
             <div className="caixa1 caixa">
-
                <label className="labelCurrency" for="caixa1">Insira o valor a ser convertido</label>
                <input className="inputCurrency" type="number" id="inputValue" classNamess="input01" />
             </div>
 
             <div class="buttons">
-
-                  <button className="btnConverter" id="btn" onClick= {ConverterDolar}>Converter em Dolar</button>
+               <button className="btnConverter" id="btn" onClick= {ConverterDolar}>Converter em Dolar</button>
                <button className="btnConverter" id="btn" onClick={ConverterEuro}>Converter em Euro</button>
             </div>
 
             <div className="caixa2 caixa">
-
                <label className="labelCurrency"  for="caixa1">Valor convertido</label>
                <div className="result">
                   <p className="input01" id="result"></p>
